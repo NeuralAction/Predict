e4587df991561f67513cce4d802cb1f9cac22bff
kidevelop <hjh010501@naver.com>
2017-12-22 22:05:56 +0900

    add PCL Project

---
+++ Predict/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+
+
+namespace Predict
+{
+    public partial class MainPage : ContentPage
+    {
+   
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
